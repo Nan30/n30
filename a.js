bddd60304271afe5bfaed821b01b94384652cf4f
Nandha30 <mail.nandha30@gmail.com>
2016-09-23 12:26:39 +0530

    Update a.js
    cghgfh

--- a.js
+++ a.js
@@ -1,1 +1,1 @@
-Customers.CustomPopup.commentDeleteConfirmation('6b28c32dddd03edfbaa6df384a43c1a40b498ee34174141d',true);
+var statusVal = getClosedLabel();assignValue('Cases','6b28c32dddd03edfaf6944840c01aa04e612657a51fd5fbe',encodeURIComponent(statusVal),'Status');
