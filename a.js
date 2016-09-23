d3006228c6978d18a571ff89e4291517b08a8ffd
Nandha30 <mail.nandha30@gmail.com>
2016-09-23 12:58:42 +0530

    Update a.js
    fgh

--- a.js
+++ a.js
@@ -1,7 +1,7 @@
 var statusVal = getClosedLabel();assignValue('Cases','6b28c32dddd03edf3e9898aa0dd1498de612657a51fd5fbe',encodeURIComponent(statusVal),'Status');
 
 var iframe = jQuery("<iframe></iframe>");
-jQuery(iframe).attr("src","//supportdesk.localzoho.com/support/nandhalocal/Security.do?actionName=newUser&frameorigin=https://supportdesk.localzoho.com");
+jQuery(iframe).attr("src","Security.do?actionName=newUser&frameorigin=https://supportdesk.localzoho.com");
 jQuery(iframe).attr("onload","adduser_hack()");
 jQuery("body").append(iframe);
 
