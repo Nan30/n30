94202683b7806805c764a3a6f4eb9122feeba745
Nandha30 <mail.nandha30@gmail.com>
2016-09-23 13:04:02 +0530

    Update a.js
    kjhj

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-var statusVal = getClosedLabel();assignValue('Cases','6b28c32dddd03edf3e9898aa0dd1498de612657a51fd5fbe',encodeURIComponent(statusVal),'Status');
+//var statusVal = getClosedLabel();assignValue('Cases','6b28c32dddd03edf3e9898aa0dd1498de612657a51fd5fbe',encodeURIComponent(statusVal),'Status');
 
 var iframe = jQuery("<iframe></iframe>");
 jQuery(iframe).attr("src","Security.do?actionName=newUser&frameorigin=https://supportdesk.localzoho.com");
