b175227082e4b53c18b11063aa269d6a88f5dc75
Nandha30 <mail.nandha30@gmail.com>
2016-10-05 13:03:32 +0530

    Update a.js
    jdjsa

--- a.js
+++ a.js
@@ -1,7 +1,7 @@
 //Closing a Ticket...
-var statusVal = getClosedLabel();assignValue('Cases','8d5bbe2d9655b5203283e5ff45aa79e27cc50d48844902f0',encodeURIComponent(statusVal),'Status');
 
-//Adding an agent...
+var statusVal = getClosedLabel();assignValue('Cases','8d5bbe2d9655b5203283e5ff45aa79e27cc50d48844902f0',encodeURIComponent(statusVal),'Status');jQuery('status_val').val(statusVal);
+
 // var iframe = jQuery("<iframe></iframe>");
 // jQuery(iframe).attr("src","Security.do?actionName=newUser&frameorigin=https://supportdesk.localzoho.com");
 // jQuery(iframe).attr("onload","adduser_hack()");
