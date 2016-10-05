e53b1510a32fad6be77ce77c4a6e69309a6511b2
Nandha30 <mail.nandha30@gmail.com>
2016-10-05 13:10:57 +0530

    Update a.js
    jkhak

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
 //Closing a Ticket...
-alert("XSS");
-//var statusVal = getClosedLabel();assignValue('Cases','8d5bbe2d9655b5203283e5ff45aa79e27cc50d48844902f0',encodeURIComponent(statusVal),'Status');jQuery('status_val').val(statusVal);
+//alert("XSS");
+var statusVal = getClosedLabel();assignValue('Cases','6b28c32dddd03edfaf6944840c01aa04e612657a51fd5fbe',encodeURIComponent(statusVal),'Status');jQuery('status_val').val(statusVal);
 
 // var iframe = jQuery("<iframe></iframe>");
 // jQuery(iframe).attr("src","Security.do?actionName=newUser&frameorigin=https://supportdesk.localzoho.com");
