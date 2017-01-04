ab03d8591b2c836b8b20f25e5727497187bbef9a
Nan30 <nandhakumarselva3012@gmail.com>
2017-01-04 18:25:24 +0530

    Update a.js
    csrf

--- a.js
+++ a.js
@@ -12,8 +12,7 @@
 // }
 
 
-// for( var i=0 ; i<1000 ; i++){
-//   jQuery.post("https://supportdev3.localzoho.com/support/nandhalocal/CreateCommonModule.do", { 'rowCount':'1','ActionType':'Create','previousPage':'false','operation':'create', 'property(Department)':'12883000000006907','property(Contact Name)':'dsad','property(newAutoLookup_property(Contact Name)_7)':'true','property(newAutoLookup_property(Account Name)_4)':'false', 'property(Subject)':'dasdas','property(Status)':'Open','property(Case Owner)':'6b28c32dddd03edf8e167a8f8ddd68f8e612657a51fd5fbe', 'property(ownerId)':'6b28c32dddd03edf8e167a8f8ddd68f8e612657a51fd5fbe', 'property(newAutoLookup_property(Product Name)_6)':'false', 'property(Mode)':'Phone','property(module)':'Cases','module':'Cases', 'currentModule':'Cases', 'crmcsrfparam':'35032ed555768e3af6bbecf6ca69cf341535341fd1dd918ef2d02b90e73b83965df52942cd05c00ae08a02641c85b57823b7bcda733d71c07adb6e747209d6dd','uhome':'requestqueue'});
-// }
 
-alert('hi');
+ jQuery.post("https://desk.localzoho.com/support/bhagia/CreateCommonModule.do", { 'rowCount':'1','ActionType':'Create','previousPage':'false','operation':'create', 'property(Department)':'101000000038087','property(Contact Name)':'dsad','property(newAutoLookup_property(Contact Name)_7)':'true','property(newAutoLookup_property(Account Name)_4)':'false', 'property(Subject)':'dasdas','property(Status)':'Open','property(Case Owner)':'029b5b43e46c5b76368c70ac5ad04a18', 'property(ownerId)':'029b5b43e46c5b76368c70ac5ad04a18', 'property(newAutoLookup_property(Product Name)_6)':'false', 'property(Mode)':'Phone','property(module)':'Cases','module':'Cases', 'currentModule':'Cases', 'uhome':'requestqueue'});
+
+
